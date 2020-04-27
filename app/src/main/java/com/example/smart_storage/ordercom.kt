dad36297d061f1e8b517e8caf47a5a893a9a209c
jittipon <ti_jittipon@hotmail.com>
2020-04-28 02:31:41 +0700

    allpage + backbutton config

--- app/src/main/java/com/example/smart_storage/ordercom.kt
+++ app/src/main/java/com/example/smart_storage/ordercom.kt
@@ -3,29 +3,31 @@ package com.example.smart_storage
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_info.*
+import kotlinx.android.synthetic.main.activity_ordercom.*
 
-class info : AppCompatActivity() {
+class ordercom : AppCompatActivity() {
 
     override fun onBackPressed() {
-        val i = Intent(this@info, storage::class.java)
+        val i = Intent(this@ordercom, info::class.java)
         startActivity(i)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_info)
+        setContentView(R.layout.activity_ordercom)
+
 
         btnback.setOnClickListener {
-            val intent = Intent(this@info,storage::class.java)
+            val intent = Intent(this@ordercom,order::class.java)
             startActivity(intent)
         }
 
-        orderBtn.setOnClickListener {
-            val intent = Intent(this@info,order::class.java)
-            startActivity(intent)
 
-        }
+
+
+
+
+
 
 
 
--- app/src/main/java/com/example/smart_storage/storage.kt
+++ app/src/main/java/com/example/smart_storage/storage.kt
@@ -3,19 +3,39 @@ package com.example.smart_storage
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_register.*
 import kotlinx.android.synthetic.main.activity_storage.*
 
 class storage : AppCompatActivity() {
+
+    lateinit var showuser: TextView
+    var backcount = 0
+    override fun onBackPressed() {
+
+        backcount = backcount + 1
+        Toast.makeText(applicationContext,"press again to logout", Toast.LENGTH_SHORT).show()
+        if (backcount>1)
+        {
+            Toast.makeText(applicationContext,"log out success", Toast.LENGTH_SHORT).show()
+            val i = Intent(this@storage, MainActivity::class.java)
+            startActivity(i)
+            backcount = 0
+        }
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_storage)
 
+        showuser = findViewById(R.id.username)
 
-        backBtn.setOnClickListener {
-            val intent = Intent(this@storage,MainActivity::class.java)
-            startActivity(intent)
-        }
+        val username = intent.getStringExtra("user")
+        showuser.text = username
+
+
         box1.setOnClickListener {
             val intent = Intent(this@storage,availablebox::class.java)
             startActivity(intent)
@@ -24,13 +44,6 @@ class storage : AppCompatActivity() {
             val intent = Intent(this@storage,availablebox::class.java)
             startActivity(intent)
         }
-
-
-
-
-
-
-
 
 
 
