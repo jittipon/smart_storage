698fd1403070c9a586f83dd8dd3992b49e493038
jittipon <ti_jittipon@hotmail.com>
2020-04-28 23:36:36 +0700

    backbutt config

--- app/src/main/java/com/example/smart_storage/MainActivity.kt
+++ app/src/main/java/com/example/smart_storage/MainActivity.kt
@@ -3,9 +3,31 @@ package com.example.smart_storage
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity() {
+
+    var doubleBackToExitOnce:Boolean = false
+
+    override fun onBackPressed() {
+        if(doubleBackToExitOnce){
+            super.onBackPressed()
+            return
+        }
+
+        this.doubleBackToExitOnce = true
+
+        //displays a toast message when user clicks exit button
+        Toast.makeText(applicationContext,"please press again to exit ",Toast.LENGTH_SHORT).show()
+        //displays the toast message for a while
+        Handler().postDelayed({
+            kotlin.run { doubleBackToExitOnce = false }
+        }, 2000)
+
+    }
 
 
 
