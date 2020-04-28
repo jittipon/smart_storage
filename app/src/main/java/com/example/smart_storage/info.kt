9d429940ea190ca82719d2304ab26c0109f5ec46
jittipon <ti_jittipon@hotmail.com>
2020-04-29 02:40:24 +0700

    add meter + anto(jittipon)

--- app/src/main/java/com/example/smart_storage/info.kt
+++ app/src/main/java/com/example/smart_storage/info.kt
@@ -3,9 +3,17 @@ package com.example.smart_storage
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.anto.Anto
+import com.example.anto.ResponseAnto
+import com.example.anto.ValueEventListener
 import kotlinx.android.synthetic.main.activity_info.*
 
 class info : AppCompatActivity() {
+
+
+    val reference = Anto.getInstance().getReference("fQ5cOLsk3e0AioI5o91oOTfAeCHmY81itFZvXrDI","LockerProject")
+    val temp = reference.addChannel("temp1")
+    val humid = reference.addChannel("Humid1")
 
     override fun onBackPressed() {
         val i = Intent(this@info, storage::class.java)
@@ -15,6 +23,59 @@ class info : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_info)
+
+        pointerSpeedometer.withTremble = false
+        pointerSpeedometer2.withTremble = false
+
+        var tempf : Float
+
+        temp.addValueEventListener(object :ValueEventListener{
+            override fun onDataChange(responseAnto: ResponseAnto) {
+                tempf = responseAnto.value.toFloat()
+                pointerSpeedometer.speedTo(tempf);
+
+                //showtemp.text = temp.toString()
+                //showtemp.text = responseAnto.value+"°C"
+            }
+
+            override fun onCancelled(dataBaseError: String) {
+
+                //showtemp.text = "Error inPut from ANto"
+            }
+
+
+
+        })
+        var humidf :Float
+
+        humid.addValueEventListener(object  : ValueEventListener{
+            override fun onDataChange(responseAnto: ResponseAnto) {
+                humidf = responseAnto.value.toFloat()
+                pointerSpeedometer2.speedTo(humidf);
+
+                //showhumid.text = responseAnto.value+"%"
+            }
+
+            override fun onCancelled(dataBaseError: String) {
+
+                //showhumid.text = "Error inPut from ANto"
+            }
+
+
+
+        })
+
+
+
+
+
+
+
+
+
+
+
+
 
         btnback.setOnClickListener {
             val intent = Intent(this@info,storage::class.java)
--- app/src/main/java/com/example/smart_storage/info2a.kt
+++ app/src/main/java/com/example/smart_storage/info2a.kt
@@ -3,10 +3,21 @@ package com.example.smart_storage
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.anto.Anto
+import com.example.anto.ResponseAnto
+import com.example.anto.ValueEventListener
 import kotlinx.android.synthetic.main.activity_availablebox2a.*
+import kotlinx.android.synthetic.main.activity_info.*
 import kotlinx.android.synthetic.main.activity_info2a.*
+import kotlinx.android.synthetic.main.activity_info2a.pointerSpeedometer
+import kotlinx.android.synthetic.main.activity_info2a.pointerSpeedometer2
 
 class info2a : AppCompatActivity() {
+
+
+    val reference = Anto.getInstance().getReference("fQ5cOLsk3e0AioI5o91oOTfAeCHmY81itFZvXrDI","LockerProject")
+    val temp = reference.addChannel("temp2")
+    val humid = reference.addChannel("Humid2")
 
 
     override fun onBackPressed() {
@@ -17,6 +28,47 @@ class info2a : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_info2a)
+
+        pointerSpeedometer.withTremble = false
+        pointerSpeedometer2.withTremble = false
+
+        var tempf : Float
+
+        temp.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(responseAnto: ResponseAnto) {
+                tempf = responseAnto.value.toFloat()
+                pointerSpeedometer.speedTo(tempf);
+
+                //showtemp.text = temp.toString()
+                //showtemp.text = responseAnto.value+"°C"
+            }
+
+            override fun onCancelled(dataBaseError: String) {
+
+                //showtemp.text = "Error inPut from ANto"
+            }
+
+
+
+        })
+        var humidf :Float
+
+        humid.addValueEventListener(object  : ValueEventListener {
+            override fun onDataChange(responseAnto: ResponseAnto) {
+                humidf = responseAnto.value.toFloat()
+                pointerSpeedometer2.speedTo(humidf);
+
+                //showhumid.text = responseAnto.value+"%"
+            }
+
+            override fun onCancelled(dataBaseError: String) {
+
+                //showhumid.text = "Error inPut from ANto"
+            }
+
+
+
+        })
 
         backBtn2a.setOnClickListener {
             val intent = Intent(this@info2a,storage::class.java)
